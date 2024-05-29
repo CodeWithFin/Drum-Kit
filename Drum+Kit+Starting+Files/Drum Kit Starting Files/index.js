d777e58aae8ebc3612417a75c5228cd0141c4a2a
Finley <finley.mwachia12@gmail.com>
2024-05-29 11:57:34 +0300

    added the timeout functions to the button to bring about the animation effect

--- Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -55,6 +55,10 @@ default:console.log(buttonInnerHTML);
 function buttonAnimation(currentKey){
 var activeButton = document.querySelector("."+ currentKey);
 activeButton.classList.add("pressed");
+
+setTimeout(function(){
+    activeButton.classList.remove("pressed");
+}, 100);
 }
 
   
